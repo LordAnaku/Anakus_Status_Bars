972a35417893b7e9e39e7820805241895696e1ba
LordAnaku <Anaku.senko@gmail.com>
2022-08-14 22:52:20 -0400

    Initial commit

---
+++ src/main/java/net/anakusenko/anakus_status_bars/AnakusStatusBarsClient.java
@@ -0,0 +1,10 @@
+package net.anakusenko.anakus_status_bars;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnakusStatusBarsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/anakusenko/anakus_status_bars/AnakusStatusBarsCore.java
@@ -0,0 +1,15 @@
+package net.anakusenko.anakus_status_bars;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AnakusStatusBarsCore implements ModInitializer {
+	public static final String MOD_ID = "anakus_status_bars";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
